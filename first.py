d394e14e01c0eb1089831d1d6e116cd09238c0a2
mehrashiv <mehrashiv@yahoo.com>
2015-04-05 17:25:11 -0700

    third commit

--- first.py
+++ first.py
@@ -3,3 +3,19 @@ class FirstClass:
         self.data = value
     def display(self):
         print(self.data)
+
+x = FirstClass() 
+y = FirstClass()
+
+x.setdata("King Arthur")
+y.setdata(3.14159)
+
+x.display()
+y.display()
+
+x.data = "New Value"
+x.display()
+
+x.anothername = "spam"
+x.display()
+
