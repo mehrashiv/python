ef5e9fe9c80bae4206c95d3dad314f2175b47c85
Shiv Mehra <mehrashiv@yahoo.com>
2015-06-08 10:37:59 -0700

    second pass

--- persons.py
+++ persons.py
@@ -5,8 +5,30 @@ class Persons():
 		self.job = job
 		self.pay = pay
 
+	def lastName(self):
+		return self.name.split()[-1]
+
+	def giveRaise(self, percent):
+		self.pay = int(self.pay * (1+percent))
+		return self.pay
+
 	def __repr__(self):
-		return(self.name, self.job, self.pay)
+		return '[Person:  %s, %s]' % (self.name, self.pay)
+
+class Manager(Persons):
+	"""docstring for ClassName"""
+	def giveRaise(self, percent, bonus):
+		Persons.giveRaise(self, percent + bonus)
 		
-bob = Persons("Bob Smith", "dev")
-print(bob)
+if __name__ == '__main__':
+
+	bob = Persons("Bob Smith", "dev")
+	sue = Persons("Sue Jone", "dev", 100000)
+	print(bob)
+	print(sue)
+	print bob.lastName(), sue.lastName()
+	print("Pay ==>" , sue.giveRaise(0.1))
+	tom = Manager('Tom Luke', 'mgr', 100000)
+	tom.giveRaise(.1,.1)
+	print(tom.pay)
+	
